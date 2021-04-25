862573f7d9210b49ce197ebc26b1feb71faf26a3
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-26 01:09:11 +0530

    Sindhu : find latest recipes in general and find latest recipes for author

--- services/recipe-service.js
+++ services/recipe-service.js
@@ -28,9 +28,19 @@ const findAllRecipesByTitle = (titleToFilter) => {
     return recipeModel.find({title: {$regex: new RegExp(titleToFilter, 'i')}})
 }
 
+const findLatestRecipes = (limit) => {
+    return recipeModel.find().sort({_id : -1}).limit(Number(limit));
+}
+
+const findLatestRecipesForAuthor = (limit, author_id) => {
+    return recipeModel.find({author_id : author_id}).sort({_id : -1}).limit(Number(limit))
+}
+
 module.exports = {
     createRecipeForUser,
     updateRecipe,
     findRecipeById,
-    findAllRecipesByTitle
+    findAllRecipesByTitle,
+    findLatestRecipes,
+    findLatestRecipesForAuthor
 }
